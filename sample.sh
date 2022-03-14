d2d8e93a79af23820780886a397168e982d1642e
deepashre <86648444+deepashre@users.noreply.github.com>
2022-03-14 20:40:41 +0530

    Delete sample.sh

--- sample.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cat > demo-sample.sh <<'EOF'
-#!/bin/bash
-sleep 30
-echo "Hello World from $(hostname)"
-EOF
-sudo chmod +x demo-sample.sh
-awsbsub -jn demo -cf demo-sample.sh Luca
-awsbstat
